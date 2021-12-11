b8385daa6e6b908edf2517411105a52c7c6d24ca
Ugur Kiymetli <ugurkiymetli@hotmail.com>
2021-12-11 13:07:32 +0300

    UI created. Wrong and correct letter functionality added. Event listener created for key presses.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import Figure from "./Components/Figure";
+import Header from "./Components/Header";
+import WrongLetters from "./Components/WrongLetters";
+import Word from "./Components/Word";
+import Popup from "./Components/Popup";
+import Notification from "./Components/Notification";
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 function App() {
-  return <div> Hangman </div>;
+  console.log(selectedWord);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [playable, setPlayable] = useState(true);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup />
+      <Notification />
+    </>
+  );
 }
 
 export default App;
